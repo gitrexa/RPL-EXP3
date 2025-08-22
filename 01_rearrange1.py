13c6509fa99ba2db3c4ebee1d8803a300b67a2e3
saran <sr2788@srmist.edu.in>
2025-08-22 15:02:32 +0530

    edited

--- 01_rearrange1.py
+++ 01_rearrange1.py
@@ -5,4 +5,4 @@ def rearrange_name(name):
 	if result==None:
 		return result
 	return "{} {}".format(result[2], result[1])
-print ("durga") 
+print ("donkey") 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RPL-EXP3
-#print
+#print athul
